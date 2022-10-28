7c97c11904e0484ce2d2ac9ce71dc644c73c3375
Justine Lo <justine@ecotree.fr>
2022-10-28 16:25:34 +0200

    add eslint

--- scripts/pages/index.js
+++ scripts/pages/index.js
@@ -1,11 +1,9 @@
 async function getPhotographers() {
-    try {
-        const response = await fetch('/data/photographers.json')
-        const data = await response.json()
-        return data.photographers
-    } catch (err) {
-        console.log('an error', err)
-    }
+    const response = await fetch('/data/photographers.json')
+        .then((response) => response.json())
+        .then((data) => data.photographers)
+        .catch((err) => console.log('an error', err))
+
     return response
 }
 
@@ -15,18 +13,18 @@ async function displayData(photographers) {
     const photographersSection = document.querySelector(".photographer_section");
 
     photographers.forEach((photographer) => {
-        const photographerModel = photographerFactory(photographer);
-        const userCardDOM = photographerModel.getUserCardDOM();
-        photographersSection.appendChild(userCardDOM);
-    });
+        const photographerModel = photographerFactory(photographer)
+        const userCardDOM = photographerModel.getUserCardDOM()
+        photographersSection.appendChild(userCardDOM)
+    })
 
-};
+}
 
 async function init() {
     // Récupère les datas des photographes
-    const photographers = await getPhotographers();
-    displayData(photographers);
-};
+    const photographers = await getPhotographers()
+    displayData(photographers)
+}
 
-init();
+init()
 
