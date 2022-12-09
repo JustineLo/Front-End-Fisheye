d14afa620181183282114545a69928bc538844fe
Justine Lo <juustine.lo@gmail.com>
2022-12-09 12:45:46 +0100

    add focus on next button media modal

--- scripts/classes.js
+++ scripts/classes.js
@@ -147,7 +147,10 @@ export async function displayModalMedia(mediaDOM) {
     modal.style.display = "flex";
     const middlePart = document.getElementById("middle_part");
     const currentMedia = mediaList.find(media => media.id == mediaDOM.getAttribute("id"));
+    const nextButton = document.getElementById("next_button"); 
     middlePart.innerHTML = mediaDOM.outerHTML + `<div class="media-title">${currentMedia.title}</div>`;
+
     modal.setAttribute("aria-hidden", "false")
     main.setAttribute("aria-hidden", "true")
+    nextButton.focus();
 }
